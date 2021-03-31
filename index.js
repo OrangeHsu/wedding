2c02b02eb1762068581f98fca0d52027dcd8991f
Tzu,chien Hsu <>
2021-03-31 18:10:14 +0800

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { AppContainer } from "react-hot-loader";
 import "react-hot-loader/patch";
 import React from "react";
 import { render } from "react-dom";
-import App from "src/components/App";
+import App from "src/App";
 
 const load = (Component) => {
   render(
@@ -17,7 +17,7 @@ load(App);
 
 // Webpack Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept("src/components/App", () => {
+  module.hot.accept("src/App", () => {
     load(App);
   });
 }
